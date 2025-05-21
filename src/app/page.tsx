161c10a1403d0c9ed7e578c6fa802cb95bcf909c
Akinori Nakata <akinori.nakata@bright-century.com>
2025-05-21 16:30:04 +0900

    ToDoList 実装（APIアクセスなし）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import ToDoListComponent from "@/components/ToDoListComponent";
+
 export default function Home() {
   return (
-    <div className="container mx-auto p-3 h-dvh border border-sky-100">
-      todolist.
+    <div className="container mx-auto h-dvh border-2 border-sky-500">
+      <header>
+        <h1 className="p-3 text-white bg-sky-500 text-3xl">to do list</h1>
+      </header>
+      <main className="">
+        <ToDoListComponent />
+      </main>
     </div>
   );
 }
